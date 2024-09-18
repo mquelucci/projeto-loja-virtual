cce30d9115eb7fe77e40fab24739bf9fcf8aee79
mquelucci <mquelucci@outlook.com>
2024-09-18 15:31:56 -0300

    update model cliente

--- server/models/cliente.go
+++ server/models/cliente.go
@@ -7,9 +7,10 @@ import (
 
 type ClienteBase struct {
 	Nome     string     `json:"nome" validate:"nonzero, nonnil" gorm:"not null"`
+	Empresa  *string    `json:"empresa"`
 	Telefone string     `json:"telefone" validade:"min=8, max=9, nonnil" gorm:"not null"`
 	Email    string     `json:"email" validade:"nonzero, nonnil" gorm:"not null"`
-	CpfCnpj  string     `json:"cpf_cnpj" validade:"nonzero, nonnil" gorm:"unique;not null"`
+	CpfCnpj  string     `json:"cpf_cnpj" validade:"nonzero, nonnil" gorm:"not null"`
 	Endereco []Endereco `json:"endereco" validate:"nonzero, nonnil" gorm:"not null"`
 }
 
