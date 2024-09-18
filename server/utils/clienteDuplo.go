faef839e6c6fddb4706569bd82b9f0f829617c5d
mquelucci <mquelucci@outlook.com>
2024-09-18 15:34:20 -0300

    verificação de duplicidade de clientes (cpf cnpj)

---
+++ server/utils/clienteDuplo.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"errors"
+
+	"github.com/mquelucci/projeto-loja-virtual/server/database"
+	"github.com/mquelucci/projeto-loja-virtual/server/models"
+)
+
+func ClienteDuplo(cpfCnpj string, editando bool, clienteParaCadastrar *models.Cliente) error {
+	var clientes []models.Cliente
+
+	if editando && cpfCnpj == clienteParaCadastrar.CpfCnpj {
+		return nil
+	} else {
+		err := database.DB.Where("cpf_cnpj = ?", cpfCnpj).Find(&clientes).Error
+		if err != nil {
+			return err
+		}
+		if len(clientes) > 0 {
+			return errors.New("Já existe um cliente com esse CPF/CNPJ")
+		}
+		return nil
+	}
+}
