c479ed4b160733f6238a27f844600305f83bfaf9
mquelucci <mquelucci@outlook.com>
2024-09-18 15:36:52 -0300

    validator endereço

--- server/models/endereco.go
+++ server/models/endereco.go
@@ -1,23 +1,33 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type EnderecoBase struct {
 	Endereco string `json:"endereco" validate:"nonzero, nonnil" gorm:"not null"`
-	Numero   int    `json:"numero" validate:"min=1, nonnil" gorm:"not null"`
+	Numero   int    `json:"numero" validate:"nonnil" gorm:"not null"`
 	Bairro   string `json:"bairro" validate:"nonzero, nonnil" gorm:"not null"`
 	CEP      int    `json:"cep" validate:"nonzero, nonnil" gorm:"not null"`
 	Cidade   string `json:"cidade" validate:"nonzero, nonnil" gorm:"not null"`
-	UF       string `json:"uf" validate:"nonzero, nonnil" gorm:"not null"`
+	UF       string `json:"uf" validate:"nonzero, nonnil, len=2" gorm:"not null"`
 }
 
 type Endereco struct {
 	gorm.Model
 	ClienteID uint   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;not null"`
 	Endereco  string `json:"endereco" validate:"nonzero, nonnil" gorm:"not null"`
-	Numero    int    `json:"numero" validate:"min=1, nonnil" gorm:"not null"`
+	Numero    int    `json:"numero" validate:"nonnil" gorm:"not null"`
 	Bairro    string `json:"bairro" validate:"nonzero, nonnil" gorm:"not null"`
 	CEP       int    `json:"cep" validate:"nonzero, nonnil" gorm:"not null"`
 	Cidade    string `json:"cidade" validate:"nonzero, nonnil" gorm:"not null"`
-	UF        string `json:"uf" validate:"nonzero, nonnil" gorm:"not null"`
+	UF        string `json:"uf" validate:"nonzero, nonnil, len=2" gorm:"not null"`
+}
+
+func ValidaEndereco(endereco *Endereco) error {
+	if err := validator.Validate(endereco); err != nil {
+		return err
+	}
+	return nil
 }
